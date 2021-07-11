9072f6cf27b3ec7e18f6a625f1b8ff6042095c0f
M0Nasser <nasseruser12@gmail.com>
2021-07-11 15:41:22 +0200

    Adding patrol enemy

--- Assets/__HyperCasualGame/Scripts/Mono Scripts/EnemyWithPistol.cs
+++ Assets/__HyperCasualGame/Scripts/Mono Scripts/EnemyWithPistol.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class EnemyWithPistol : MonoBehaviour
 {
+    [SerializeField] Transform[] points;
+    [SerializeField] float enemySpeed;
+
+    int index; 
     private Animator animator;
-    private CharacterController characterController;
+    private Rigidbody rb;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        index = 0;
+        rb = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        
+        EnemyPatrol();
+    }
+
+    private void EnemyPatrol()
+    {
+        if (transform.position != points[index].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[index].position, enemySpeed * Time.deltaTime);
+            transform.LookAt(points[index].position, Vector3.up);
+            animator.SetBool("isEnemyWalking", true);
+        }
+        else
+        {
+            index = (index + 1) % points.Length;
+        }
     }
 }
